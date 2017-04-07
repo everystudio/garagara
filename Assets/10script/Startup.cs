b4826c4b3ea6bb7db4dd80950a53b3d5f760016c
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-04-07 17:55:29 +0900

    残りをコミット

---
+++ Assets/10script/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startup : MonoBehaviour {
+
+	public void OnClickStartButton()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
